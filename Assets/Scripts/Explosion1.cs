77f3d0c0d2acf4b8cf8def4639bdb724b87ad6b0
calmcalmdown <44122665+calmcalmdown@users.noreply.github.com>
2019-07-30 23:49:03 +0200

    Update Explosion1.cs

--- Assets/Scripts/Explosion1.cs
+++ Assets/Scripts/Explosion1.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class Explosion1 : MonoBehaviour {
 
+	void Start () {
+		Destroy(gameObject,0.25f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
 	// Use this for initialization
 	
 }
